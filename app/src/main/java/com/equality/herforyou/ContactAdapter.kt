f5b2b43a007ed3b24c76eeac7c609c9f63c28542
Dona <dona.denkey_dandjein@edu.devinci.fr>
2023-06-13 16:46:45 +0200

    update ContactAdapter

--- app/src/main/java/com/equality/herforyou/ContactAdapter.kt
+++ app/src/main/java/com/equality/herforyou/ContactAdapter.kt
@@ -1,32 +1,36 @@
 package com.equality.herforyou
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ContactAdapter(var mList: List<ContactData>) : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
+class ContactAdapter(private var mList: List<ContactData>) : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
 
     inner class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val number: TextView = itemView.findViewById(R.id.phoneNumberOrName)
-        val name : TextView = itemView.findViewById(R.id.contactName)
-        val description : TextView = itemView.findViewById(R.id.contactDesc)
+        val contactNumber: TextView = itemView.findViewById(R.id.phoneNumberOrName)
+        val contactName : TextView = itemView.findViewById(R.id.contactName)
+        val contactDescription : TextView = itemView.findViewById(R.id.contactDesc)
     }
 
-    fun setFilteredList(mList: List<ContactData>){
-        this.mList = mList
-        notifyDataSetChanged()
-    }
+//    fun setFilteredList(mList: List<ContactData>){
+//        this.mList = mList
+//        notifyDataSetChanged()
+//    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_contact, parent,false)
+        return ContactViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
-
+        holder.contactNumber.text = mList[position].number.toString()
+        holder.contactName.text = mList[position].name
+        holder.contactDescription.text = mList[position].description
     }
 
     override fun getItemCount(): Int {
-
+        return mList.size
     }
 }
